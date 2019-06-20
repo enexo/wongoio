65858b126edc3690cede8d001599b2bd44c19590
Derek McDaniel <derek.mcdaniel@mheducation.com>
2019-06-20 12:50:18 -0400

    gitignore file
    Password hash

--- includes/login.php
+++ includes/login.php
@@ -2,39 +2,42 @@
 <?php session_start(); ?>
 
 <?php
-if(isset($_POST['login'])) {
-    echo "Found";
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if (isset($_POST['login'])) {
 
-    $username = mysqli_real_escape_string($connection, $username);
-    $password = mysqli_real_escape_string($connection, $password);
+	$username = $_POST['username'];
+	// This data should be hashed in some sort. I recommend hasing it in the db
+	// https://www.sitepoint.com/hashing-passwords-php-5-5-password-hashing-api/
+	$password = $_POST['password'];
 
-    $query = "SELECT * FROM users WHERE username = '{$username}'";
-    $selectUserQuery = mysqli_query($connection, $query);
-    if(!$selectUserQuery) {
-        die("Query Failed" . mysqli_error($connection));
-    }
-    while($row = mysqli_fetch_array($selectUserQuery)) {
-        $db_id = $row['userId'];
-        $db_username = $row['username'];
-        $db_password = $row['password'];
-        $db_firstName = $row['firstName'];
-        $db_lastName = $row['lastName'];
-        $db_role = $row['role'];
-    }
-//check if UN and PW are correct. If not, back to index.php
-    if($username === $db_username && $password === $db_password) {
-        $_SESSION['username'] = $db_username;
-        $_SESSION['firstName'] = $db_firstName;
-        $_SESSION['lastName'] = $db_lastName;
-        $_SESSION['role'] = $db_role;
+	$username = mysqli_real_escape_string($connection, $username);
+	$password = mysqli_real_escape_string($connection, $password);
 
-        header("Location: ../dataEntry.php");
-    }
+	$query = "SELECT * FROM users WHERE username = '{$username}'";
+	$selectUserQuery = mysqli_query($connection, $query);
 
-    else {
-        header("Location: ../index.php");
-    }
+	if (!$selectUserQuery) {
+		die("Query Failed" . mysqli_error($connection));
+	}
+
+	while ($row = mysqli_fetch_array($selectUserQuery)) {
+		$db_id = $row['userId'];
+		$db_username = $row['username'];
+		$db_password = $row['password'];
+		$db_firstName = $row['firstName'];
+		$db_lastName = $row['lastName'];
+		$db_role = $row['role'];
+	}
+
+	//check if UN and PW are correct. If not, back to index.php
+	if ($username === $db_username && password_verify($password, $db_password)) {
+		$_SESSION['username'] = $db_username;
+		$_SESSION['firstName'] = $db_firstName;
+		$_SESSION['lastName'] = $db_lastName;
+		$_SESSION['role'] = $db_role;
+
+		header("Location: ../dataEntry.php");
+	} else {
+		header("Location: ../index.php");
+	}
 }
 ?>
